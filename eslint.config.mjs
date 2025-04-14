22dab693f8549137d0a28b387a0a9455f927bfe2
csychenci <1443629502@qq.com>
2025-04-14 14:59:12 +0800

    feat: init project

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+// 覆盖 eslint 本身的规则配置
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+// 用 prettier 来接管修复代码
+
+export default defineConfig([
+  tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"], rules:{
+      // ...eslintConfigPrettier,
+        "prettier/prettier": "error",
+        "no-case-declarations": "off",
+        "no-constant-condition": "off",
+        "@typescript-eslint/ban-ts-comment": "off",
+      }
+    
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: {
+      globals: globals.node, parser: tseslint.parser, parserOptions: {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+      }
+    },
+  },
+  eslintPluginPrettierRecommended
+]);
